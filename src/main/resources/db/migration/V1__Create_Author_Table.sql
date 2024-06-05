c2fc6fea0248b40a7d6b8dc1318236a87de43987
Vibhor Pandey <vibhor.vibhu@gmail.com>
2024-06-05 21:06:58 +0530

    Jooq Setup

--- src/main/resources/db/migration/V1__Create_Author_Table.sql
+++ src/main/resources/db/migration/V1__Create_Author_Table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE AUTHOR (
     id SERIAL PRIMARY KEY,
+    author_id VARCHAR(10) UNIQUE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     birthday DATE NOT NULL,
